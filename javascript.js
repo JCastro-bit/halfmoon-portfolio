9891f50560a99897753ff620dc787d9bdf5c5654
javier castro <javiercastro27@outlook.es>
2023-04-20 14:23:54 -0500

    🔃 update libraries

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// Dark mode toggle demo
+function toggleDemo() {
+    halfmoon.toggleDarkMode();
+}
+
+       
+       
+Swal.fire({
+    title: 'Hello!👋',
+    text: 'I appreciate your patience as I work to create a better online experience, thank you for visiting!',
+    imageUrl: 'https://casttro.mx/assets/img/marginalia-programming.gif',
+    imageWidth: 340,
+    imageHeight: 340,
+    imageAlt: 'Man typing',
+})
